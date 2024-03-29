185b33888e345d7a83b43686a47d9710e5693482
elonj <56976129+elonj@users.noreply.github.com>
2024-03-29 06:03:23 +0000

    Update 唐人街影视_DIY.js

--- js/唐人街影视_DIY.js
+++ js/唐人街影视_DIY.js
@@ -1,6 +1,6 @@
 var rule={
     title:'唐人街影视_DIY',
-    host:'https://trjyy.com/',
+    host:'https://trjyingshi.com/',
     // url:'/vod/show/id/fyclass/page/fypage.html',
     url:'/vod/show/id/fyfilter.html',
     filterable:1,//是否启用分类筛选,
